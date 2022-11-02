a3abbf2e960280c57c6c35c557af892aedcff042
Nahom Demessie <nahomnegdem@gmail.com>
2022-11-02 17:35:37 -0500

    MWA Week One Day 3 Task one done

---
+++ mwa/w1d3/http_to_express/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname,'view')));
+app.get('/',function (req, res) {
+    res.sendFile('view/index.html');
+});
+
+app.post('/',function (req, res) {
+    res.status(200).send(JSON.stringify({title:"The Matrix"}));
+});
+
+app.listen(3434);
